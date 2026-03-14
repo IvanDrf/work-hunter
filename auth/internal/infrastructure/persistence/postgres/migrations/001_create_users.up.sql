44951ab64418a66f7cc6336054d1c4aa641e8243
IvanDrf <ivandrgmr@gmail.com>
2026-03-15 01:23:48 +0300

    [back:fix]: update migrations, username -> email and added verificated status

--- auth/internal/infrastructure/persistence/postgres/migrations/001_create_users.up.sql
+++ auth/internal/infrastructure/persistence/postgres/migrations/001_create_users.up.sql
@@ -2,10 +2,11 @@ BEGIN;
 
 CREATE TABLE IF NOT EXISTS users (
     user_id UUID PRIMARY KEY,
-    username TEXT NOT NULL UNIQUE,
-    hashed_password TEXT NOT NULL
+    email TEXT NOT NULL UNIQUE,
+    hashed_password TEXT NOT NULL,
+    verificated BOOL DEFAULT FALSE
 );
 
-CREATE INDEX IF NOT EXISTS indx_username ON users(username);
+CREATE INDEX IF NOT EXISTS indx_email ON users(email);
 
 COMMIT;
