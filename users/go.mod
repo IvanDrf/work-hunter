5aefdaec11d06a4ba6fae88fe7489f0623f922b6
ivanSaichkin <ivan@LAPTOP-N0DUVN16.localdomain>
2026-03-19 20:57:58 +0300

    [back:dev]: Add user model and DTO for users

--- users/go.mod
+++ users/go.mod
@@ -3,3 +3,5 @@ module github.com/IvanDrf/workk-hunter/pkg/users
 go 1.25.0
 
 require go.yaml.in/yaml/v3 v3.0.4
+
+require github.com/google/uuid v1.6.0 // indirect
---
+++ users/internal/domain/user/user.go
@@ -0,0 +1,103 @@
+package user
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+// user status
+type UserStatus string
+
+const (
+	UserStatusActive   UserStatus = "active"
+	UserStatusInactive UserStatus = "inactive"
+	UserStatusBlocked  UserStatus = "blocked"
+	UserStatusDeleted  UserStatus = "deleted"
+)
+
+// user role
+type UserRole string
+
+const (
+	UserRoleUser      UserRole = "user"
+	UserRoleModerator UserRole = "moderator"
+	UserStatusAdmin   UserRole = "admin"
+)
+
+type User struct {
+	ID          uuid.UUID `db:"id"`
+	Username    string    `db:"username"`
+	Email       string    `db:"email"`
+	FirstName   string    `db:"first_name"`
+	LastName    string    `db:"last_name"`
+	PhoneNumber string    `db:"phone_number"`
+	AvatarURL   string    `db:"avatar_url"`
+
+	Status UserStatus `db:"status"`
+	Role   UserRole   `db:"role"`
+
+	Metadata map[string]string `db:"metadata"`
+
+	CreatedAt time.Time `db:"created_at"`
+	UpdatedAt time.Time `db:"updatet_at"`
+}
+
+func NewUser(req *CreateUserRequest) *User {
+	now := time.Now()
+	return &User{
+		ID:          uuid.New(),
+		Username:    req.Username,
+		Email:       req.Email,
+		FirstName:   req.FirstName,
+		LastName:    req.LastName,
+		PhoneNumber: req.PhoneNumber,
+
+		Status: UserStatusActive,
+		Role:   UserRoleUser,
+
+		Metadata: make(map[string]string),
+
+		CreatedAt: now,
+		UpdatedAt: now,
+	}
+}
+
+func (u *User) UpdateUser(req *UpdateUserRequest) {
+	if req.FirstName != "" {
+		u.FirstName = req.FirstName
+	}
+	if req.LastName != "" {
+		u.LastName = req.LastName
+	}
+	if req.PhoneNumber != "" {
+		u.PhoneNumber = req.PhoneNumber
+	}
+	if req.AvatarURL != "" {
+		u.AvatarURL = req.AvatarURL
+	}
+
+	if req.Metadata != nil {
+		u.Metadata = req.Metadata
+	}
+
+	u.UpdatedAt = time.Now()
+}
+
+// DTO for creating user
+type CreateUserRequest struct {
+	Username    string
+	Email       string
+	FirstName   string
+	LastName    string
+	PhoneNumber string
+}
+
+// DTO for updating user
+type UpdateUserRequest struct {
+	FirstName   string
+	LastName    string
+	PhoneNumber string
+	AvatarURL   string
+	Metadata    map[string]string
+}
