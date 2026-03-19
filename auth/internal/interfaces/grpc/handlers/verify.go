ae361a488307968d1fd2b347ad46461df819b850
IvanDrf <ivandrgmr@gmail.com>
2026-03-19 18:27:02 +0300

    [back:fix]: fix handlers

--- auth/internal/interfaces/grpc/handlers/verify.go
+++ auth/internal/interfaces/grpc/handlers/verify.go
@@ -10,14 +10,17 @@ import (
 	"google.golang.org/grpc/status"
 )
 
-func (h *Handler) VerifyEmail(ctx context.Context, email *auth_api.Email) (*auth_api.JwtTokens, error) {
-	access, refresh, err := h.verificationService.VerifyEmail(ctx, email.Email)
+func (h *Handler) VerifyEmail(ctx context.Context, token *auth_api.VerifToken) (*auth_api.JwtTokens, error) {
+	access, refresh, err := h.verificationService.VerifyEmailByToken(ctx, token.Token)
 
 	var e models.Error
 	if errors.As(err, &e) {
 		switch e.Code {
 		case models.ErrCodeUserNotFound:
 			return nil, status.Error(codes.NotFound, e.Message)
+
+		case models.ErrOutdatedToken:
+			return nil, status.Error(codes.DeadlineExceeded, e.Message)
 
 		case models.ErrCodeInternal:
 			return nil, status.Error(codes.Internal, e.Message)
