40eaf7509fb913b3634be9b8639b947aa2bd38d2
IvanDrf <ivandrgmr@gmail.com>
2026-03-26 14:59:17 +0300

    [back:dev]: add logging in auth service

--- auth/internal/infrastructure/service/auth.go
+++ auth/internal/infrastructure/service/auth.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"context"
+	"log/slog"
 
 	"github.com/IvanDrf/work-hunter/auth/internal/domain/models"
 	"github.com/IvanDrf/work-hunter/auth/internal/domain/ports/jwt"
@@ -28,6 +29,7 @@ func (a *AuthService) Close() {
 func (a *AuthService) RegisterUser(ctx context.Context, email string, password string) (string, string, error) {
 	_, err := a.userRepo.FindUser(ctx, email)
 	if err == nil {
+		slog.Info("RegisterUser user with that email already exists")
 		return "", "", models.Error{
 			Message: "user with that email already exists",
 			Code:    models.ErrCodeUserAlreadyExists,
@@ -36,11 +38,13 @@ func (a *AuthService) RegisterUser(ctx context.Context, email string, password s
 
 	user, err := models.NewUser(email, password)
 	if err != nil {
+		slog.Error("RegisterUser error", slog.String("error", err.Error()))
 		return "", "", err
 	}
 
 	err = a.userRepo.CreateUser(ctx, user)
 	if err != nil {
+		slog.Error("RegisterUser error", slog.String("error", err.Error()))
 		return "", "", models.Error{
 			Message: "can't register new user",
 			Code:    models.ErrCodeInternal,
@@ -49,18 +53,21 @@ func (a *AuthService) RegisterUser(ctx context.Context, email string, password s
 
 	access, refresh, err := a.jwter.CreateTokens(user.ID, user.Verificated)
 	if err != nil {
+		slog.Error("RegisterUser error", slog.String("error", err.Error()))
 		return "", "", models.Error{
 			Message: "can't create jwt tokens for user",
 			Code:    models.ErrCodeInternal,
 		}
 	}
 
+	slog.Info("RegisterUser success")
 	return access, refresh, nil
 }
 
 func (a *AuthService) LoginUser(ctx context.Context, email string, password string) (string, string, error) {
 	user, err := a.userRepo.FindUser(ctx, email)
 	if err != nil {
+		slog.Error("LoginUser error", slog.String("error", err.Error()))
 		return "", "", models.Error{
 			Message: "user with that email doesn't exists",
 			Code:    models.ErrCodeUserNotFound,
@@ -68,6 +75,7 @@ func (a *AuthService) LoginUser(ctx context.Context, email string, password stri
 	}
 
 	if !rules.IsPasswordsSame(password, user.HashedPassword) {
+		slog.Info("LoginUser incorrect password")
 		return "", "", models.Error{
 			Message: "invalid password",
 			Code:    models.ErrCodeInvalidPassword,
@@ -76,36 +84,42 @@ func (a *AuthService) LoginUser(ctx context.Context, email string, password stri
 
 	access, refresh, err := a.jwter.CreateTokens(user.ID, user.Verificated)
 	if err != nil {
+		slog.Error("LoginUser error", slog.String("error", err.Error()))
 		return "", "", models.Error{
 			Message: "can't create jwt tokens for user",
 			Code:    models.ErrCodeInternal,
 		}
 	}
 
+	slog.Info("LoginUser success")
 	return access, refresh, nil
 }
 
 func (a *AuthService) RefreshTokens(ctx context.Context, refresh string) (string, string, error) {
 	access, refresh, err := a.jwter.RefreshTokens(refresh)
 	if err != nil {
+		slog.Error("RefreshTokens error", slog.String("error", err.Error()))
 		return "", "", models.Error{
 			Message: "invalid jwt token",
 			Code:    models.ErrCodeInvalidJWT,
 		}
 	}
 
+	slog.Info("RefreshTokens success")
 	return access, refresh, nil
 }
 
 func (a *AuthService) GetTokenPayload(ctx context.Context, access string) (*models.JwtPayload, error) {
 	id, verificated, err := a.jwter.GetPayload(access)
 	if err != nil {
+		slog.Error("GetTokenPayload error", slog.String("error", err.Error()))
 		return nil, models.Error{
 			Code:    models.ErrCodeInvalidJWT,
 			Message: "invalid jwt token",
 		}
 	}
 
+	slog.Info("GetTokenPayload success")
 	return &models.JwtPayload{
 		ID:          id,
 		Verificated: verificated,
