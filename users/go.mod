661f62f8475ec1a2d0b6ac9ca60cedd7ab3867e5
ivanSaichkin <ivan@LAPTOP-N0DUVN16.localdomain>
2026-04-12 14:03:24 +0300

    [back:fix]: fix logging in service

--- users/go.mod
+++ users/go.mod
@@ -1,6 +1,6 @@
 module github.com/IvanDrf/work-hunter/users
 
-go 1.25.0
+go 1.25.1
 
 require go.yaml.in/yaml/v3 v3.0.4
 
@@ -11,4 +11,16 @@ require (
 
 require github.com/lib/pq v1.10.9
 
-require github.com/jmoiron/sqlx v1.4.0
+require (
+	github.com/IvanDrf/work-hunter/pkg/user-api v0.0.0-20260401110126-ac223fd7dc2f
+	github.com/jmoiron/sqlx v1.4.0
+	google.golang.org/grpc v1.80.0
+	google.golang.org/protobuf v1.36.11
+)
+
+require (
+	golang.org/x/net v0.49.0 // indirect
+	golang.org/x/sys v0.40.0 // indirect
+	golang.org/x/text v0.33.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20260120221211-b8f7ae30c516 // indirect
+)
--- users/internal/infrastructure/service/user.go
+++ users/internal/infrastructure/service/user.go
@@ -30,7 +30,7 @@ func NewUserService(repo repository.UserRepository, log *logger.Logger) *UserSer
 }
 
 func (s *UserService) CreateProfile(ctx context.Context, req *dto.CreateUserRequest) (*dto.UserResponse, error) {
-	log := s.log.With("scope", "infrastructure/service/CreateProfile")
+	log := s.log.With(slog.String("scope", "infrastructure/service/CreateProfile"))
 
 	uuid, err := parseUUID(req.ID, log)
 	if err != nil {
@@ -47,19 +47,19 @@ func (s *UserService) CreateProfile(ctx context.Context, req *dto.CreateUserRequ
 			Code:    models.ErrCodeUserAlreadyExists,
 		}
 	}
-	log.Debug("user model created successfully", "id", user.ID.String())
+	log.Debug("user model created successfully", slog.String("id", user.ID.String()))
 
 	if err := s.repo.CreateUser(ctx, user); err != nil {
-		log.Error("failed to create user", "error", err)
+		log.Error("failed to create user", slog.String("error", err.Error()))
 		return nil, err
 	}
-	log.Info("user created successfully", "id", user.ID.String())
+	log.Info("user created successfully", slog.String("id", user.ID.String()))
 
 	return modelToResp(user, log)
 }
 
 func (s *UserService) GetProfile(ctx context.Context, id string) (*dto.UserResponse, error) {
-	log := s.log.With("scope", "infrastructure/service/GetProfile")
+	log := s.log.With(slog.String("scope", "infrastructure/service/GetProfile"))
 
 	uuid, err := parseUUID(id, log)
 	if err != nil {
@@ -68,30 +68,30 @@ func (s *UserService) GetProfile(ctx context.Context, id string) (*dto.UserRespo
 
 	user, err := s.repo.GetUserByID(ctx, uuid)
 	if err != nil {
-		log.Error("failed to get user by id", "error", err)
+		log.Error("failed to get user by id", slog.String("error", err.Error()))
 
 		return nil, err
 	}
-	log.Info("user found successfully", "id", user.ID.String())
+	log.Info("user found successfully", slog.String("id", user.ID.String()))
 
 	return modelToResp(user, log)
 }
 
 func (s *UserService) GetProfileByUsername(ctx context.Context, username string) (*dto.UserResponse, error) {
-	log := s.log.With("scope", "infrastructure/service/GetProfileByUsername")
+	log := s.log.With(slog.String("scope", "infrastructure/service/GetProfileByUsername"))
 
 	user, err := s.repo.GetUserByUsername(ctx, username)
 	if err != nil {
-		log.Error("failed to get user by username", "error", err)
+		log.Error("failed to get user by username", slog.String("error", err.Error()))
 		return nil, err
 	}
-	log.Info("user found successfully", "id", user.ID.String())
+	log.Info("user found successfully", slog.String("id", user.ID.String()))
 
 	return modelToResp(user, log)
 }
 
 func (s *UserService) UpdateProfile(ctx context.Context, req *dto.UpdateUserRequest) (*dto.UserResponse, error) {
-	log := s.log.With("scope", "infrastructure/service/UpdateProfile")
+	log := s.log.With(slog.String("sscope", "infrastructure/service/UpdateProfile"))
 
 	uuid, err := parseUUID(req.ID, log)
 	if err != nil {
@@ -100,23 +100,23 @@ func (s *UserService) UpdateProfile(ctx context.Context, req *dto.UpdateUserRequ
 
 	user, err := s.repo.GetUserByID(ctx, uuid)
 	if err != nil {
-		log.Error("failed to get user by id", "error", err)
+		log.Error("failed to get user by id", slog.String("error", err.Error()))
 		return nil, err
 	}
-	log.Debug("user found successfully", "id", user.ID.String())
+	log.Debug("user found successfully", slog.String("id", user.ID.String()))
 
 	user.UpdateUser(req.FirstName, req.LastName, req.PhoneNumber, req.AvatarURL, req.Metadata)
 	if err := s.repo.UpdateUser(ctx, user); err != nil {
-		log.Error("failed to update user", "error", err)
+		log.Error("failed to update user", slog.String("error", err.Error()))
 		return nil, err
 	}
-	log.Info("user updated successfully", "id", user.ID.String())
+	log.Info("user updated successfully", slog.String("id", user.ID.String()))
 
 	return modelToResp(user, log)
 }
 
 func (s *UserService) DeleteProfile(ctx context.Context, id string) error {
-	log := s.log.With("scope", "infrastructure/service/DeleteProfile")
+	log := s.log.With(slog.String("scope", "infrastructure/service/DeleteProfile"))
 
 	uuid, err := parseUUID(id, log)
 	if err != nil {
@@ -125,10 +125,10 @@ func (s *UserService) DeleteProfile(ctx context.Context, id string) error {
 
 	user, err := s.repo.GetUserByID(ctx, uuid)
 	if err != nil {
-		log.Error("failed to get user by id", "error", err)
+		log.Error("failed to get user by id", slog.String("error", err.Error()))
 		return err
 	}
-	log.Debug("user successfully found", "id", user.ID.String())
+	log.Debug("user successfully found", slog.String("id", user.ID.String()))
 
 	if user.Status == "deleted" {
 		err = s.repo.DeleteUser(ctx, uuid, true)
@@ -137,16 +137,16 @@ func (s *UserService) DeleteProfile(ctx context.Context, id string) error {
 	}
 
 	if err != nil {
-		log.Error("failed to delete user", "error", err)
+		log.Error("failed to delete user", slog.String("error", err.Error()))
 		return err
 	}
-	log.Info("user deleted successfully", "user", user)
+	log.Info("user deleted successfully", slog.String("id", user.ID.String()))
 
 	return nil
 }
 
 func (s *UserService) ListUsers(ctx context.Context, req *dto.ListUsersRequest) (*dto.ListUsersResponse, error) {
-	log := s.log.With("scope", "infrastructure/service/ListUsers")
+	log := s.log.With(slog.String("scope", "infrastructure/service/ListUsers"))
 
 	enabledFields := map[string]struct{}{
 		"id": {}, "username": {}, "email": {}, "first_name": {}, "last_name": {},
@@ -176,7 +176,7 @@ func (s *UserService) ListUsers(ctx context.Context, req *dto.ListUsersRequest) 
 
 	values, err := url.ParseQuery(req.SearchQuery)
 	if err != nil {
-		log.Error("failed to parse query", "error", err)
+		log.Error("failed to parse query", slog.String("error", err.Error()))
 		return nil, &models.Error{
 			Message: fmt.Sprintf("failed to parse search query: %v", err),
 			Code:    models.ErrCodeInvalidRequest,
@@ -186,7 +186,7 @@ func (s *UserService) ListUsers(ctx context.Context, req *dto.ListUsersRequest) 
 	for key, val := range values {
 		_, ok := enabledFields[key]
 		if !ok {
-			log.Error("cannot use this field", "field", key)
+			log.Error("cannot use this field", slog.String("field", key))
 			return nil, &models.Error{
 				Message: fmt.Sprintf("cannot use this field: %s", key),
 				Code:    models.ErrCodeInvalidRequest,
@@ -197,14 +197,14 @@ func (s *UserService) ListUsers(ctx context.Context, req *dto.ListUsersRequest) 
 			params[key] = val[0]
 		}
 	}
-	log.Debug("params created successfully", "params", params)
+	log.Debug("params created successfully")
 
 	users, totalCount, err := s.repo.ListUsers(ctx, params)
 	if err != nil {
-		log.Error("failed to list users", "error", err)
+		log.Error("failed to list users", slog.String("error", err.Error()))
 		return nil, err
 	}
-	log.Info("users listed successfully", "count", len(users))
+	log.Info("users listed successfully", slog.Int("count", len(users)))
 
 	usersResp := make([]*dto.UserResponse, 0, len(users))
 	for _, val := range users {
@@ -229,7 +229,7 @@ func (s *UserService) ListUsers(ctx context.Context, req *dto.ListUsersRequest) 
 }
 
 func (s *UserService) UpdateUserStatus(ctx context.Context, req *dto.UpdateUserStatusRequest) (*dto.UserResponse, error) {
-	log := s.log.With("scope", "infrastructure/service/UpdateUserStatus")
+	log := s.log.With(slog.String("scope", "infrastructure/service/UpdateUserStatus"))
 
 	id, err := parseUUID(req.ID, log)
 	if err != nil {
@@ -241,15 +241,15 @@ func (s *UserService) UpdateUserStatus(ctx context.Context, req *dto.UpdateUserS
 	wg.Go(func() {
 		user, errGet := s.repo.GetUserByID(ctx, id)
 		if err != nil {
-			log.Error("failed to get user", "error", errGet)
+			log.Error("failed to get user", slog.String("error", errGet.Error()))
 		}
-		log.Debug("user found successfully", "id", user.ID.String())
+		log.Debug("user found successfully", slog.String("id", user.ID.String()))
 
 		user.Status = rules.UserStatus(req.Status)
 	})
 
 	if err = s.repo.UpdateUserStatus(ctx, id, rules.UserStatus(req.Status)); err != nil {
-		log.Error("failed to update user status", "error", err)
+		log.Error("failed to update user status", slog.String("error", err.Error()))
 		return nil, err
 	}
 
@@ -262,31 +262,32 @@ func (s *UserService) UpdateUserStatus(ctx context.Context, req *dto.UpdateUserS
 
 func (s *UserService) Close() {
 	s.repo.Close()
+	s.log.Info("connection closed successfully")
 }
 
 func parseUUID(id string, log *slog.Logger) (uuid.UUID, error) {
 	uuid, err := uuid.Parse(id)
 	if err != nil {
-		log.Error("failed to parse uuid from string", "error", err)
+		log.Error("failed to parse uuid from string", slog.String("error", err.Error()))
 		return uuid, &models.Error{
 			Message: fmt.Sprintf("failed to parse uuid from string: %v", err),
 			Code:    models.ErrCodeInvalidRequest,
 		}
 	}
-	log.Debug("uuid parsed successfully", "uuid", uuid)
+	log.Debug("uuid parsed successfully")
 	return uuid, nil
 }
 
 func modelToResp(user *models.User, log *slog.Logger) (*dto.UserResponse, error) {
 	metadata := make(map[string]string)
 	if err := json.Unmarshal(user.Metadata, &metadata); err != nil {
-		log.Error("failed to unmarshal json data", "error", err)
+		log.Error("failed to unmarshal json data", slog.String("error", err.Error()))
 		return nil, &models.Error{
 			Message: fmt.Sprintf("failed to unmarshal json data: %v", err),
 			Code:    models.ErrCodeInternal,
 		}
 	}
-	log.Debug("user converted successfully", "id", user.ID.String())
+	log.Debug("user converted successfully", slog.String("id", user.ID.String()))
 
 	return &dto.UserResponse{
 		ID:          user.ID.String(),
