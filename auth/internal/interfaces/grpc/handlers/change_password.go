177edafd0dda8030d8cbc41f0329475c808d5901
IvanDrf <ivandrgmr@gmail.com>
2026-04-12 12:53:43 +0300

    [back:dev]: add ChangeUserPassword handler

---
+++ auth/internal/interfaces/grpc/handlers/change_password.go
@@ -0,0 +1,33 @@
+package handlers
+
+import (
+	"context"
+	"errors"
+
+	"github.com/IvanDrf/work-hunter/auth/internal/domain/models"
+	auth_api "github.com/IvanDrf/work-hunter/pkg/auth-api"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+func (h *Handler) ChangeUserPassword(ctx context.Context, req *auth_api.ChangePasswordRequest) (*auth_api.ChangePasswordStatus, error) {
+	err := h.authService.ChangeUserPassword(ctx, req.Access, req.Old, req.New)
+
+	var e models.Error
+	if errors.As(err, &e) {
+		switch e.Code {
+		case models.ErrCodeInvalidJWT, models.ErrCodeInvalidPassword:
+			return nil, status.Error(codes.InvalidArgument, e.Message)
+
+		case models.ErrCodeUserNotFound:
+			return nil, status.Error(codes.NotFound, e.Message)
+
+		case models.ErrCodeInternal:
+			return nil, status.Error(codes.Internal, e.Message)
+		}
+	}
+
+	return &auth_api.ChangePasswordStatus{
+		Changed: true,
+	}, nil
+}
