5f1dffddbf91ca2b58135ecbafe08f90b72f230c
IvanDrf <ivandrgmr@gmail.com>
2026-05-03 14:57:32 +0300

    [back:dev]: add __init__.py in service dependencies for better importing

---
+++ vacancy/src/infrastructure/service/dependencies/__init__.py
@@ -0,0 +1,7 @@
+from src.infrastructure.service.dependencies.cache import ICache
+from src.infrastructure.service.dependencies.repo import IVacancyRepo
+
+__all__ = [
+    'ICache',
+    'IVacancyRepo'
+]
