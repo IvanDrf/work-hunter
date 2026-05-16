b4e4bb8b16c96ff8be1bf40e0601bffc6bf5d5b5
IvanDrf <ivandrgmr@gmail.com>
2026-05-17 01:28:54 +0300

    [back:tests]: add tests for rules: has_right_to_vacancy

--- vacancy/tests/unit/rules/conftest.py
+++ vacancy/tests/unit/rules/conftest.py
@@ -1,9 +1,10 @@
-from uuid import UUID
+from uuid import UUID, uuid4
 
 from pytest import fixture
 
+from src.domain.models import VacancyORM
 from src.domain.schemas.user import UserInfo
-from src.domain.types.enums import UserRole
+from src.domain.types.enums import UserRole, VacancyStatus
 
 
 @fixture(scope="package")
@@ -71,5 +72,60 @@ def unspecified_user_info(user_id) -> tuple[UserInfo, ...]:
             user_role=UserRole.UNSPECIFIED,
             user_id=user_id,
             verificated=False,
+        ),
+    )
+
+
+@fixture(scope="function")
+def vacancies(user_id) -> tuple[tuple[VacancyORM, UserInfo | None, bool], ...]:
+    author_id = uuid4()
+
+    return (
+        # published vacancies
+        (
+            VacancyORM(status=VacancyStatus.PUBLISHED, author_id=author_id),
+            UserInfo(user_role=UserRole.ADMIN, user_id=user_id, verificated=True),
+            True,
+        ),
+        (
+            VacancyORM(status=VacancyStatus.PUBLISHED, author_id=author_id),
+            UserInfo(user_role=UserRole.EMPLOYEE, user_id=user_id, verificated=True),
+            True,
+        ),
+        (
+            VacancyORM(status=VacancyStatus.PUBLISHED, author_id=author_id),
+            UserInfo(user_role=UserRole.EMPLOYER, user_id=user_id, verificated=True),
+            True,
+        ),
+        (
+            VacancyORM(status=VacancyStatus.PUBLISHED, author_id=author_id),
+            None,
+            True,
+        ),
+        # moderating vacancies, only author and moderator should see vacancy
+        (  # admin
+            VacancyORM(status=VacancyStatus.MODERATING, author_id=author_id),
+            UserInfo(user_role=UserRole.ADMIN, user_id=user_id, verificated=True),
+            True,
+        ),
+        (  # author
+            VacancyORM(status=VacancyStatus.MODERATING, author_id=user_id),
+            UserInfo(user_role=UserRole.EMPLOYER, user_id=user_id, verificated=True),
+            True,
+        ),
+        (
+            VacancyORM(status=VacancyStatus.MODERATING, author_id=author_id),
+            UserInfo(user_role=UserRole.EMPLOYEE, user_id=user_id, verificated=True),
+            False,
+        ),
+        (
+            VacancyORM(status=VacancyStatus.MODERATING, author_id=author_id),
+            UserInfo(user_role=UserRole.EMPLOYER, user_id=user_id, verificated=True),
+            False,
+        ),
+        (  # no user info
+            VacancyORM(status=VacancyStatus.MODERATING, author_id=author_id),
+            None,
+            False,
         ),
     )
--- vacancy/tests/unit/rules/test_vacancy.py
+++ vacancy/tests/unit/rules/test_vacancy.py
@@ -1,8 +1,13 @@
 from pytest import mark
 
-from src.domain.rules.vacancy import is_vacancy_id_valid
+from src.domain.rules.vacancy import has_right_to_vacancy, is_vacancy_id_valid
 
 
 @mark.parametrize("vacancy_id", [1, -5, 2, 19, -3, 0, 23, -39, 123])
 def test_is_vacancy_id_valid(vacancy_id) -> None:
     assert is_vacancy_id_valid(vacancy_id) is (vacancy_id > 0)
+
+
+def test_has_right_to_vacancy(vacancies) -> None:
+    for vacancy, user_info, res in vacancies:
+        assert has_right_to_vacancy(vacancy, user_info) is res
