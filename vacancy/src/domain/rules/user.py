0bb74471546a673179fb1ceb93421f96c7017e40
IvanDrf <ivandrgmr@gmail.com>
2026-05-18 00:33:38 +0300

    [back:dev]: add is_user_vacancy_author func

--- vacancy/src/domain/rules/user.py
+++ vacancy/src/domain/rules/user.py
@@ -7,3 +7,7 @@ def is_user_admin(user_info: UserInfo) -> bool:
 
 def is_user_employer(user_info: UserInfo) -> bool:
     return user_info.user_role == UserRole.EMPLOYER
+
+
+def is_user_vacancy_author(vacancy_id: int, user_info: UserInfo) -> bool:
+    return vacancy_id == user_info.user_id
