78696e5eda823f06bac16c7d81f4b822f16c9749
IvanDrf <ivandrgmr@gmail.com>
2026-07-02 18:31:26 +0300

    [tests]: fixed redis timeout

--- validation/src/core/config/database.py
+++ validation/src/core/config/database.py
@@ -8,4 +8,4 @@ class RedisConfig(BaseConfig):
     redis_port: int = Field(default=6379, validation_alias="REDIS_PORT")
 
     redis_database: int = Field(default=0, ge=0, validation_alias="REDIS_DB")
-    redis_timeout: float = Field(default=0, gt=0, validation_alias="REDIS_TIMEOUT")
+    redis_timeout: float = Field(default=0.1, gt=0, validation_alias="REDIS_TIMEOUT")
