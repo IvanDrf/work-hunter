b7b650c9ab945d33578e95cbf47c10b75bdadcbf
IvanDrf <ivandrgmr@gmail.com>
2026-07-05 14:36:18 +0300

    [feat]: add api-limiter in validate_metro method and added tests for limiter

--- validation/tests/e2e/test_app.py
+++ validation/tests/e2e/test_app.py
@@ -1,6 +1,7 @@
 from fastapi import status
 from fastapi.testclient import TestClient
 
+from src.api.metro import limiter
 from src.app.main import app
 from src.core.config import CONFIG
 
@@ -19,10 +20,30 @@ def test_validate_metro(cities_and_metro_json) -> None:
     with TestClient(app=app) as client:
         for city, stations in cities_and_metro_json:
             for station in stations:
+                limiter._storage.reset()  # no 429 error
+
                 assert_valid_request(client, city, station)
                 assert_request_without_api_key(client, city, station)
                 assert_request_invalid_body(client, city, station)
                 assert_request_not_found_metro(client, city, 2 * station)
+
+
+def test_validate_metro_limiter(cities_and_metro_json) -> None:
+    RPS = 30
+    requests_amount = 0
+
+    with TestClient(app=app) as client:
+        for city, stations in cities_and_metro_json:
+            for station in stations:
+                response = client.post(
+                    url="/api/metro",
+                    headers={"api-key": CONFIG.api_key, "Content-type": "application/json"},
+                    json={"city": city, "metro": station},
+                )
+
+                requests_amount += 1
+                if requests_amount >= RPS:
+                    assert response.status_code == status.HTTP_429_TOO_MANY_REQUESTS
 
 
 def assert_valid_request(client: TestClient, city: str, station: str) -> None:
