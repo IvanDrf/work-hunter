aaf690489c09a9b4769db0d10108b004cffeca4c
IvanDrf <ivandrgmr@gmail.com>
2026-07-09 11:06:43 +0300

    [tests]: add tests for city validation api

--- validation/tests/e2e/test_metro_api.py
+++ validation/tests/e2e/test_metro_api.py
@@ -6,16 +6,6 @@ from src.app.main import app
 from src.core.config import CONFIG
 
 
-def test_health(requests_amount: int) -> None:
-    for _ in range(requests_amount):
-        with TestClient(app=app) as client:
-            response = client.get(url="/health")
-            assert response.status_code == status.HTTP_200_OK
-            assert response.json() == {"status": "AVAILABLE"}
-
-            response.close()
-
-
 def test_validate_metro(cities_and_metro_json) -> None:
     with TestClient(app=app) as client:
         for city, stations in cities_and_metro_json:
@@ -38,7 +28,7 @@ def test_validate_metro_limiter(cities_and_metro_json) -> None:
             for station in stations:
                 response = client.post(
                     url="/api/metro",
-                    headers={"api-key": CONFIG.api_key, "Content-type": "application/json"},
+                    headers={"api-key": CONFIG.api_key},
                     json={"city": city, "metro": station},
                 )
 
@@ -48,14 +38,14 @@ def test_validate_metro_limiter(cities_and_metro_json) -> None:
                 else:
                     assert response.status_code == status.HTTP_204_NO_CONTENT
 
-                if requests_amount > 3 * RPS:
+                if requests_amount > 3 * RPS:  # limiter timeout
                     return
 
 
 def assert_valid_request(client: TestClient, city: str, station: str) -> None:
     response = client.post(
         url="/api/metro",
-        headers={"api-key": CONFIG.api_key, "Content-type": "application/json"},
+        headers={"api-key": CONFIG.api_key},
         json={"city": city, "metro": station},
     )
 
@@ -66,7 +56,6 @@ def assert_valid_request(client: TestClient, city: str, station: str) -> None:
 def assert_request_without_api_key(client: TestClient, city: str, station: str) -> None:
     response = client.post(
         url="/api/metro",
-        headers={"Content-type": "application/json"},
         json={"city": city, "metro": station},
     )
 
@@ -77,7 +66,7 @@ def assert_request_without_api_key(client: TestClient, city: str, station: str) 
 def assert_request_invalid_body(client: TestClient, city: str, station: str) -> None:
     response = client.post(
         url="/api/metro",
-        headers={"api-key": CONFIG.api_key, "Content-type": "application/json"},
+        headers={"api-key": CONFIG.api_key},
         json={"city_": city, "metro_": station},
     )
 
@@ -88,7 +77,7 @@ def assert_request_invalid_body(client: TestClient, city: str, station: str) -> 
 def assert_request_not_found_metro(client: TestClient, city: str, station: str) -> None:
     response = client.post(
         url="/api/metro",
-        headers={"api-key": CONFIG.api_key, "Content-type": "application/json"},
+        headers={"api-key": CONFIG.api_key},
         json={"city": city, "metro": station},
     )
 
