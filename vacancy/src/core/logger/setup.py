a29942e787a584327e72aa38ffb9629836f10d0c
IvanDrf <ivandrgmr@gmail.com>
2026-08-03 09:54:41 +0300

    [dev]: add correctly session close in unit of work

--- vacancy/src/core/logger/setup.py
+++ vacancy/src/core/logger/setup.py
@@ -12,8 +12,4 @@ levels = {
 def setup_logger(level: str, log_file: str | None = None) -> None:
     level = level.lower()
 
-    if log_file:
-        basicConfig(level=levels[level], filename=log_file, filemode="a", format="%(asctime)s %(levelname)s %(message)s")
-
-    else:
-        basicConfig(level=levels[level], format="%(asctime)s %(name)s %(levelname)s %(message)s")
+    basicConfig(level=levels[level], filename=log_file, format="%(asctime)s %(name)s %(levelname)s %(message)s")
